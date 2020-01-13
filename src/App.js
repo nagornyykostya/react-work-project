957ceb9d8de2441204dd5e56e11dbdcab258fd38
MyMac <nagornyykostya@gmail.com>
2020-01-13 19:19:06 +0200

    added react-router and sign in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import {Forbidden} from './components/forbidden/Forbidden'
+import {SignIn} from './components/signIn/SignIn'
+
 function App() {
   return (
+    <Router>
     <div>
-      <Forbidden />
+      <Route exact path="/" component={SignIn} />
+      <Route path="/403forbidden" component={Forbidden} />
     </div>
+    </Router>
   );
 }
 
