53be2cfd1714950ce3010a59c0024b35629f8d0d
MyMac <nagornyykostya@gmail.com>
2020-01-13 19:38:10 +0200

    sign in page design added

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './SignIn.css';
 
 export const SignIn = () => (
-    <di>
-        Sign in form
-    </di>
+    <div className="signin-container">
+        <h1>Welcome</h1>
+        <form className="signin-form">
+            <input className="input-email" type="email" placeholder="Email address"></input>
+            <button className="button-submit" type="submit">Next</button>
+        </form>
+    </div>
 );
