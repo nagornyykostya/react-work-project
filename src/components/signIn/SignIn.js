4c2c42d217b5c9a7d95d37c2463c6d186fc54a40
MyMac <nagornyykostya@gmail.com>
2020-01-15 12:34:26 +0200

    added handleChange and handleSubmit to Sign In component

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.css';
 
-export const SignIn = () => (
-    <div className="signin-container">
-        <h1>Welcome</h1>
-        <form className="signin-form">
-            <input className="input-email" type="email" placeholder="Email address"></input>
-            <button className="button-submit" type="submit">Next</button>
-        </form>
-    </div>
-);
+export function SignIn() {
+
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+    }
+    return (
+        <div className="signin-container">
+            <h1>Welcome</h1>
+            <form className="signin-form" onSubmit={handleSubmit}>
+                <input className="input-email" type="email" placeholder="Email address" value={email} onChange={handleChange}></input>
+                <button className="button-submit" type="submit" value="Submit">Next</button>
+            </form>
+        </div>
+    )
+};
